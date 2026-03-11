bfcd53938a3a855c4b0943eab18cb6035518fd59
skvortsovmark <skvortsovmark55@gmail.com>
2026-03-11 19:26:11 +0700

    Update api_constants.dart

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,5 @@
 final class ApiConstants {
-  // ВНИМАНИЕ: ключ вшит в приложение по вашему запросу.
-  // Это небезопасно для продакшена (ключ легко извлекается из APK/IPA).
+
   static const geminiApiKey = 'AIzaSyDb1hNvfupkmSZZokBq8muFygM5W6048ac';
 
   static const geminiBaseUrl = 'https://generativelanguage.googleapis.com';
